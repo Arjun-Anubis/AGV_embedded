c5b8df43fc05917cb8aeef828e8d743468941d25
Arjun Ghosh <anubiz512z@gmail.com>
2026-03-27 19:35:44 +0530

    added a new build

--- task2/testbench.v
+++ task2/testbench.v
@@ -1,35 +1,48 @@
+`timescale 1ns/1ps
+
 module testbench();
-	reg reset = 0;
-	reg clock = 0;
-	wire done;
-	reg [7:0] angle = 42;
+    reg  reset = 0;
+    reg  clock = 0;
+    wire done;
+    reg  [7:0] angle;
+    wire [11:0] x, y;
 
+    CORDIC c( .clock(clock), .reset(reset), .angle(angle), .done(done), .x(x), .y(y) );
 
-	wire [11:0] x, y;
+    always #5 clock = ~clock;
 
-	CORDIC c( .clock( clock ), .reset( reset ), .angle( angle ), .done( done ), .x( x ), .y( y ) );
+    initial begin
+        $dumpfile("cordic.vcd");
+        $dumpvars(0, testbench);
+    end
 
+    task run_cordic;
+        input [7:0] a;
+        begin
+            angle = a;
+            reset = 1;
+            @(posedge clock); #1;
+            reset = 0;
+            @(posedge done); #1;
+            $display("%d, %d, %d", a, x, y);
+        end
+    endtask
 
-	initial begin
-		forever begin
-			#10 clock = ~clock;
-		end
-	end
+    integer i;
+    initial begin
+        // single tests
+		$display ("angle, x, y");
+        run_cordic(0);
+        run_cordic(42);
+        run_cordic(72);
+        run_cordic(90);
+        run_cordic(255);
 
-	initial begin
-		reset = 1;
-		@(posedge clock);
-		reset = 0;
-		@(posedge done) begin
-			$display("Output is x = %d, y = %d", x, y);
-		end
-		angle = 72;
-		reset = 1;
-		@(posedge clock);
-		reset = 0;
-		@(posedge done) begin
-			$display("Output is x = %d, y = %d", x, y);
-		end
+        // sweep
+        for (i = 0; i < 256; i = i + 1)
+            run_cordic(i);
 
-	end
+        $finish;
+    end
+
 endmodule
